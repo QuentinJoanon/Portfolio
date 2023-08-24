056ef2d22b2e74fa88bdccad778bc6069b777e10
Quentin Joanon <121891265+QuentinJoanon@users.noreply.github.com>
2023-08-24 10:15:28 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Quentin JOANON : Développeur Web JavaScript',
   description:
-    'Bienvenue sur mon portfolio de développeur web JavaScript ! Découvrez une collection inspirante de projets interactifs et créatifs, démontrant des compétences approfondies en développement web. Explorez des applications réactives et des interfaces utilisateur intuitives, conçues avec les dernières technologies JavaScript, telles que React, Node.js, Next.js et Redux. Plongez dans le monde captivant du développement web et laissez-vous impressionner par des réalisations innovantes.',
+    'Bienvenue sur mon portfolio de développeur web JavaScript ! Découvrez une collection inspirante de projets interactifs et créatifs, démontrant des compétences approfondies en développement web. Explorez des applications réactives et des interfaces utilisateur intuitives, conçues avec les dernières technologies JavaScript, telles que React, Node.js, Next.js et Redux.',
 };
 
 export default function Home() {
